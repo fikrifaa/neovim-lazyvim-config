70a28c06055617cfb8e0d44c96e1d4960a6934e6
pikri <fafyaps123@gmail.com>
2025-07-19 21:20:41 +0700

    update-config-nvim-lazyvim
    highlight sintak buat blade

--- lua/plugins/blade.lua
+++ lua/plugins/blade.lua
@@ -2,5 +2,19 @@ return {
   {
     "jwalton512/vim-blade",
     ft = "blade",
+    config = function()
+      -- Matikan highlight treesitter khusus blade
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = "blade",
+        callback = function()
+          vim.treesitter.stop() -- stop treesitter biar fallback ke vim syntax
+          vim.cmd([[set syntax=blade]])
+
+          -- Tambahin highlight manual buat variabel
+          vim.cmd([[syntax match phpVariable /\$\w\+/ containedin=htmlString,htmlTag]])
+          vim.cmd([[highlight phpVariable guifg=#e06c75]])
+        end,
+      })
+    end,
   },
 }
